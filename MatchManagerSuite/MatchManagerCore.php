b54bd41de8ae42b36382aeaa8278d4f1a9b9f910
Beu <benoit@virtit.fr>
2023-10-09 10:20:33 +0200

    bump version

--- MatchManagerSuite/MatchManagerCore.php
+++ MatchManagerSuite/MatchManagerCore.php
@@ -37,7 +37,7 @@ use ManiaControl\Callbacks\TimerListener; // for pause
 class MatchManagerCore implements CallbackListener, CommandListener, TimerListener, CommunicationListener, Plugin {
 
 	const PLUGIN_ID											= 152;
-	const PLUGIN_VERSION									= 4.4;
+	const PLUGIN_VERSION									= 4.5;
 	const PLUGIN_NAME										= 'MatchManager Core';
 	const PLUGIN_AUTHOR										= 'Beu';
 
