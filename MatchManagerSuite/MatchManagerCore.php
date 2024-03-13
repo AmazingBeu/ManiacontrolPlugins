65e8fb2454bfb04a43b4247100239d483ef43777
Beu <benoit@virtit.fr>
2024-03-13 16:40:38 +0100

    bump version to bypass site issue

--- MatchManagerSuite/MatchManagerCore.php
+++ MatchManagerSuite/MatchManagerCore.php
@@ -38,7 +38,7 @@ use ManiaControl\Callbacks\TimerListener; // for pause
 class MatchManagerCore implements CallbackListener, CommandListener, TimerListener, CommunicationListener, Plugin {
 
 	const PLUGIN_ID											= 152;
-	const PLUGIN_VERSION									= 4.10;
+	const PLUGIN_VERSION									= 5.0;
 	const PLUGIN_NAME										= 'MatchManager Core';
 	const PLUGIN_AUTHOR										= 'Beu';
 
